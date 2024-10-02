7f9c815bc317675c73f6d8124c15ce98ca2f25af
tushar <tusharkashayp9368@gmail.com>
2024-10-02 22:15:13 +0530

    make filter the products and Home button on screen using useLocation(seach,pathname)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 
 // import Nav from "./Component/Nav"
 import Home from "./Component/Home"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
 import Details from "./Component/Details"
+import { Link } from "react-router-dom"
 function App() {
+const {search , pathname} =   useLocation()
   return (
     <div className="h-screen w-screen flex" >
+
+      {(pathname != "/" || search.length > 0) && (
+        <Link to="/" className="text-red-300 absolute left-[17%] top-[3%] " >Home</Link>
+      )}
+
+
+
      <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/Details/:id" element={<Details/>}/>
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,12 +1,35 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Nav from './Nav';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ProductContext } from '../utils/Context';
 import Loading from './Loading';
+import axios from '../utils/Axios';
 
 function Home() {
   const [products] = useContext(ProductContext); // Extracting products from context
   // console.log(products);
+
+  const { search } = useLocation();// This the method to implement the fetch nav category data from the URL
+  const category = decodeURIComponent(search.split("=")[1]);
+  
+  const [filteredProducts, setfilteredProducts] = useState(null);
+
+
+  const getproductscategory = async ()=>{
+      try {
+        const {data} = await axios.get(`/products/category/${category}`);
+        setfilteredProducts(data)
+      } catch (error) {
+        console.log(error)
+      }
+  }
+
+  useEffect(()=>{
+    if(!filteredProducts || category == "undefined") setfilteredProducts(products);
+    if(category != "undefined") getproductscategory()
+  },[category , products]);
+
+  // console.log(filteredProducts)
 
   return (
     products ? (
@@ -14,7 +37,7 @@ function Home() {
         <Nav />
         <div className="h-full w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
 
-          {products.map((p,i)=>(
+          { filteredProducts && filteredProducts.map((p,i)=>(
              <Link key={i} to={`/details/${p.id}`} className="card bg-zinc-100 mr-3 mb-3 p-3 border shadow rounded w-[18%] h-[30vh] flex flex-col justify-center items-center">
              <div
                className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-center bg-no-repeat"
